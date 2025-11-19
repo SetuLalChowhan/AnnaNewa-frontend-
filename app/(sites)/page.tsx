8f730906fe64185800a2d952724cd37f11cf221a
Setulal Chowhan <setulalchowhan@gmail.com>
2025-11-19 17:11:30 +0600

    fixed issue

--- app/(sites)/page.tsx
+++ app/(sites)/page.tsx
@@ -8,7 +8,7 @@ import ProductForBuy from "@/components/home/ProductForBuy";
 import ProductForSell from "@/components/home/ProductForSell";
 import Testimonial from "@/components/home/Testimonial";
 import WhyGoDirectSection from "@/components/home/WhyGoDirectSection";
-import { useEmailStore } from "@/providers/useState";
+
 import React from "react";
 
 const page = () => {
